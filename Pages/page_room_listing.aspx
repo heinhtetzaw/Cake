45fe0b3d812b87ef7b6d175560a289fe8939e38b
Hein Htet Zaw <heinhtetzaw@live.com>
2012-07-18 22:23:54 +0800

    Starting the database

--- Pages/page_room_listing.aspx
+++ Pages/page_room_listing.aspx
@@ -3,5 +3,8 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+<asp:GridView ID="gridview_rooms_list" runat="server">
+</asp:GridView>
 </asp:Content>
 
--- Pages/page_room_listing.aspx.cs
+++ Pages/page_room_listing.aspx.cs
@@ -9,6 +9,10 @@ public partial class Pages_page_room_listing : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (IsPostBack) return;
+        RoomsDataContext _roomsDataContext = new RoomsDataContext();
+        List<room> rooms=  (from c in _roomsDataContext.rooms   select c).ToList();
+        gridview_rooms_list.DataSource = rooms;
+        gridview_rooms_list.DataBind();
     }
 }
